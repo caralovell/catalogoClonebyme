f4d383d37f802acd66d106d5fac58cb358f6c4c3
caralovell <168685711+caralovell@users.noreply.github.com>
2026-06-05 09:39:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/situmCatalogo/",
+  base: "/catalogoClonebyme/",
   server: {
     host: "::",
     port: 8080,
